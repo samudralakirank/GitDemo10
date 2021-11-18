718b1e59f107e603a553b119207a76b1c9d88ae1
Kiran <kiransamudrala95@gmail.com>
2021-11-18 15:40:13 +0530

    Initial Commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace GitDemo10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
